7315a24ff45b5413ba435258ed25416b8131755d
Oshinko <osnk@renjaku.jp>
2021-11-07 20:46:44 +0900

    Update

--- setup.py
+++ setup.py
@@ -32,9 +32,6 @@ exec(about_script, about)
 readme = (here / 'README.md').read_text(encoding='utf8')
 readme_mimetype = 'text/markdown'
 
-# import json
-# setuptools.setup = lambda **kwargs: print(json.dumps(kwargs, indent=2))
-
 setuptools.setup(
     name=about['name'],
     version=about['version'],
